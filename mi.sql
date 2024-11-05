ae3ea24eb97a93c7c349fa6f574dfb75b5ed93c7
Hugo <hugo.rimorcity@gmail.com>
2024-11-05 19:26:21 +0100

    Solucionados los problemas de la consulta y ya esta lista la tabla canciones

--- mi.sql
+++ mi.sql
@@ -107,10 +107,9 @@ create table canciones(
 );
 
 insert into canciones (titulo, titulo_disco, año_publicacion_disco, duracion)
-    select distinct temp_canciones.titulo, temp_discos.nombre, temp_discos.fecha_lanzamiento::integer, 
+    select distinct on (temp_canciones.titulo, temp_discos.nombre, temp_discos.fecha_lanzamiento)temp_canciones.titulo, temp_discos.nombre, temp_discos.fecha_lanzamiento::integer, 
         to_char(
             make_interval(hours => 0, mins => split_part(temp_canciones.duracion, ':', 1)::int, secs => split_part(temp_canciones.duracion, ':', 2)::int),'HH24:MM:SS')::time 
             from temp_canciones join temp_discos on temp_canciones.id = temp_discos.id;  
-select * from canciones;
 
 rollback;
